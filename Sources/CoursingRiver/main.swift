e13df95367dd59ef34706578f431fd364ab867d3
Lizzie Siegle <esiegle@brynmawr.edu>
2018-12-12 15:00:04 -0800

    save user SMS input to local text file under project

--- Sources/CoursingRiver/main.swift
+++ Sources/CoursingRiver/main.swift
@@ -3,6 +3,8 @@ import Foundation
 
 let app = try Application()
 let router = try app.make(Router.self)
+var audienceMadeArr: [String] = []
+//let drop = try Droplet()
 
    // Create an HTTP Response with the twiml as the body.
    // var resp = HTTPResponse(status: .ok, body: twiml)
@@ -19,6 +21,43 @@ router.post("serversidesms") { req -> String in
     guard words.contains(body) else {
         return "send one of these words: \(words)"
     }
+    audienceMadeArr.append(body)
+    print(audienceMadeArr)
+    let fileName = "audiencemade.txt"
+    var filePath = ""
+    let dirs: [String] = NSSearchPathForDirectoriesInDomains(FileManager.SearchPathDirectory.desktopDirectory, FileManager.SearchPathDomainMask.allDomainsMask, true)
+    if dirs.count > 0 {
+        let dir = dirs[0] //docs dir
+        filePath = dir.appending("/" + "ios/CoursingRiver/CoursingRiver/" + fileName)
+        print("Local path = \(filePath)")
+    } else {
+        print("could not find local dir to store file")
+        //return
+    }
+    let formattedArr = (audienceMadeArr.map{String($0)}).joined(separator: ",")
+    let fileContentToWrite = String(formattedArr)
+    do {
+        try fileContentToWrite.write(toFile: filePath, atomically: false, encoding: String.Encoding.utf8)
+    }
+    catch let err as NSError {
+        print("error took place: \(err)")
+    }
+//    if let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
+//
+//        let fileURL = dir.appendingPathComponent(file)
+//
+//        //writing
+//        do {
+//            try body.write(to: fileURL, atomically: false, encoding: .utf8)
+//        }
+//        catch {/* error handling here */}
+//
+//        //reading
+//        do {
+//            let text2 = try String(contentsOf: fileURL, encoding: .utf8)
+//        }
+//        catch {/* error handling here */}
+//    }
     return "nice! got it!"
 }
 try app.run()
